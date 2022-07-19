e7c9ad62bc36363d1d9e641993fad73f94dbdc4b
olegrewko <olegrewko@gmail.com>
2022-07-19 19:21:19 +0300

    ui

--- bw800-master/docs/js/app.js
+++ bw800-master/docs/js/app.js
@@ -1,1 +1,12 @@
-$(document).foundation()
+let cart = {
+  'art1965': 2,
+  'art1964': 2
+}
+
+document.onclick = event => {
+  // console.log(event.target.classlist);
+  if (event.target.classList.contains('count__up')) {
+    // plusFunction(event.target.dadaSet.id);
+    console.log(event.target.dataset.id);
+  }
+}
