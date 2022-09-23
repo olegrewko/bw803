a272c095f23e654ce32ab0fc8aea5bce286bf1c1
olegrewko <olegrewko@gmail.com>
2022-09-23 17:37:03 +0300

    u

--- docs/js/slider.js
+++ docs/js/slider.js
@@ -1,28 +1,66 @@
-const swiper = new Swiper('.slider-block', {
-    slidesPerView: 1,
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
+// const swiper = new Swiper('.slider-block', {
+//     slidesPerView: 1,
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
   
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-  const sliderNavItems = document.querySelectoAll('.slider-nav__item');
-  sliderNavItems.forEach((el, index)=> {
-    el.setAttribute('data-index', index);
-  })
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
+//   const sliderNavItems = document.querySelectoAll('.slider-nav__item');
+//   sliderNavItems.forEach((el, index)=> {
+//     el.setAttribute('data-index', index);
+//   })
+document.addEventListener('DOMContentLoaded', () => {
 
+	let mySwiper = new Swiper('.slider-block', {
+		slidesPerView: 1,
+	})
+
+	const maxItems = 5;
+	const sliderNavItems = document.querySelectorAll('.slider-nav__item');
+	const sliderNav = document.querySelector('.slider-nav');
+
+	sliderNavItems.forEach((el, index) => {
+		el.setAttribute('data-index', index);
+
+		el.addEventListener('click', (e) => {
+			const index = parseInt(e.currentTarget.dataset.index);
+			console.log(index)
+			mySwiper.slideTo(index);
+		});
+	});
+
+	const showMore = () => {
+		let childenLength = sliderNav.children.length;
+		console.log(childenLength)
+		if (childenLength > maxItems) {
+			sliderNav.insertAdjacentHTML('beforeend', `
+				<div class="btn-center">
+					<button class="modal-open">Еще ${childenLength - maxItems}</button>
+				</div>
+			`);
+			document.querySelectorAll(`.slider-nav__item:nth-child(n+${maxItems + 1})`).forEach(el => {el.style.display = 'none';});
+		}
+
+		const modalOpen = document.querySelector('.modal-open');
+		modalOpen.addEventListener('click', () => {new GraphModal().open('one');});
+	};
+
+	showMore();
+
+});
   
